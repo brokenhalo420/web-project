b0db0e95e7d43db6b26b5a61c25eb097d8ff0a31
Mario Nikolov <m.nikolov1501@gmail.com>
2022-06-12 23:58:39 +0300

    setup + get invites

---
+++ backend/config/setup.sql
@@ -0,0 +1,298 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 12, 2022 at 10:58 PM
+-- Server version: 10.4.22-MariaDB
+-- PHP Version: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `system`
+--
+CREATE DATABASE IF NOT EXISTS `system` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `system`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `chats`
+--
+
+CREATE TABLE `chats` (
+  `id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `chats`
+--
+
+INSERT INTO `chats` (`id`) VALUES
+(1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `invites`
+--
+
+CREATE TABLE `invites` (
+  `id` int(255) NOT NULL,
+  `queue_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `invites`
+--
+
+INSERT INTO `invites` (`id`, `queue_id`) VALUES
+(1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messages`
+--
+
+CREATE TABLE `messages` (
+  `id` int(255) NOT NULL,
+  `text` text NOT NULL,
+  `private` tinyint(1) NOT NULL,
+  `chat_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`id`, `text`, `private`, `chat_id`) VALUES
+(1, 'Здравей!', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queues`
+--
+
+CREATE TABLE `queues` (
+  `id` int(255) NOT NULL,
+  `name` varchar(300) NOT NULL,
+  `url` varchar(500) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `queues`
+--
+
+INSERT INTO `queues` (`id`, `name`, `url`) VALUES
+(1, 'Web presentation', 'https://meet.google.com/baa-thot-nua'),
+(2, 'Algebra presentation', 'https://meet.google.com/xrs-stau-dno'),
+(3, 'OS Presentation', 'https://meet.google.com/yfk-ount-dka');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `queueusers`
+--
+
+CREATE TABLE `queueusers` (
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `queue_id` int(255) NOT NULL,
+  `position` int(255) NOT NULL,
+  `startTime` time NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `userinvites`
+--
+
+CREATE TABLE `userinvites` (
+  `id` int(255) NOT NULL,
+  `user_id` int(255) NOT NULL,
+  `invite_id` int(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `userinvites`
+--
+
+INSERT INTO `userinvites` (`id`, `user_id`, `invite_id`) VALUES
+(1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(255) NOT NULL,
+  `firstname` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `email` varchar(320) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `type` enum('TEACHER','STUDENT') NOT NULL DEFAULT 'STUDENT'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `username`, `password`, `type`) VALUES
+(1, 'Milen', 'Petrov', 'milen.petrov@gmail.com', 'milen1406', '5249f3b45cd4b5ba3efa537e87060edf538a81696dfe5d05b57cd6bdb3a3bcc8', 'TEACHER'),
+(2, 'Mario', 'Nikolov', 'mario@gmail.com', 'mariogn12', '2b149399bf55697bd116635ef91e04cb4d5928e00c59bb948b6b75fcc89a1d67', 'STUDENT'),
+(3, 'Ivan', 'Kostadinov', 'ivank@gmail.com', 'ivan13kost', 'eace38e7a80b565c8e53f7a2f0405ca2b628dd73d2cd5ca01d20330cffae80f8', 'STUDENT');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `chats`
+--
+ALTER TABLE `chats`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `invites`
+--
+ALTER TABLE `invites`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `queue_id` (`queue_id`);
+
+--
+-- Indexes for table `messages`
+--
+ALTER TABLE `messages`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `chat_id` (`chat_id`);
+
+--
+-- Indexes for table `queues`
+--
+ALTER TABLE `queues`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `queueusers`
+--
+ALTER TABLE `queueusers`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id` (`user_id`),
+  ADD UNIQUE KEY `queue_id` (`queue_id`);
+
+--
+-- Indexes for table `userinvites`
+--
+ALTER TABLE `userinvites`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `user_id of userinvites` (`user_id`),
+  ADD UNIQUE KEY `invite_id of userinvites` (`invite_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`,`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `chats`
+--
+ALTER TABLE `chats`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `invites`
+--
+ALTER TABLE `invites`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `messages`
+--
+ALTER TABLE `messages`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `queues`
+--
+ALTER TABLE `queues`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `queueusers`
+--
+ALTER TABLE `queueusers`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `userinvites`
+--
+ALTER TABLE `userinvites`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=373;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `chats`
+--
+ALTER TABLE `chats`
+  ADD CONSTRAINT `chats_ibfk_1` FOREIGN KEY (`id`) REFERENCES `queues` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `invites`
+--
+ALTER TABLE `invites`
+  ADD CONSTRAINT `invites_ibfk_1` FOREIGN KEY (`queue_id`) REFERENCES `queues` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `messages`
+--
+ALTER TABLE `messages`
+  ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`chat_id`) REFERENCES `chats` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `queueusers`
+--
+ALTER TABLE `queueusers`
+  ADD CONSTRAINT `queueusers_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `queueusers_ibfk_2` FOREIGN KEY (`queue_id`) REFERENCES `queues` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `userinvites`
+--
+ALTER TABLE `userinvites`
+  ADD CONSTRAINT `userinvites_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  ADD CONSTRAINT `userinvites_ibfk_2` FOREIGN KEY (`invite_id`) REFERENCES `invites` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
