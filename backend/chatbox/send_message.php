f014681c572594af684f22d25bc6f3b8eb922d35
Georgi Boychev <georgiboychevv@gmail.com>
2022-06-12 03:05:12 +0300

    hard earned successes

--- backend/chatbox/send_message.php
+++ backend/chatbox/send_message.php
@@ -6,16 +6,21 @@ try{
 }
 catch(PDOException $pd) {
     echo json_encode(['pls kill me' => 'i am serious']);
+    
 }
 
 $con = $db->getConnection();
 
-$data = json_decode(file_get_contents('php://input'));
-echo json_encode($data);
-//prowerka za null
+$data = json_decode(file_get_contents('php://input'),true);
+$query = "INSERT INTO messages (text, private) VALUES (:text, :private)";
 
+try {
+    $result = $con->prepare($query);
+    $result->execute($data);
+}
+catch(PDOException $pd) {
+    throw new Exception('ss');
+}
 
-
-echo json_encode(['status' => 'oisdjfos']);
-
+echo json_encode(["status","success"]);
 ?>
